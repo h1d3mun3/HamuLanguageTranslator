0032535e02324b93c27481ead7a6c9a2442b0fc9
Hidemune Takahashi <h1d3mun3@gmail.com>
2024-03-30 15:27:08 +0900

    ハム語変換器を追加

--- Sources/HamuLanguageTranslateor/HamuLanguageTranslateor.swift
+++ Sources/HamuLanguageTranslateor/HamuLanguageTranslateor.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct HamuLanguageTranslateor {
+    public func apply(source: String) -> String {
+        var result = source
+        var containedWords = [HamuWord]()
+        for word in HamuWord.allCases {
+            if source.contains(word.getNaturalWord()) {
+                containedWords.append(word)
+            }
+        }
+        
+        containedWords.forEach({result.replace($0.getNaturalWord(), with: $0.getHamuWord()) })
+
+        return result
+    }
+}
