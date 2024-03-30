c07a5889b972b7bdeafe3ddcbb6432f21969d6b0
Hidemune Takahashi <h1d3mun3@gmail.com>
2024-03-30 23:14:56 +0900

    UnitTestをより具体的なものに変更

--- Tests/HamuLanguageTranslatorTests/HamuLanguageTranslatorTests.swift
+++ Tests/HamuLanguageTranslatorTests/HamuLanguageTranslatorTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class HamuLanguageTranslatorTests: XCTestCase {
     func testExample() throws {
-        let sources = "あいさつ"
-        let expected = "はむはー"
+        let sources = "あいさつ！このライブラリはHamuLanguageTranslatorというんだ！君の入力したテキストをハム語に変換するよ！君の素敵なアプリと協力するしてくれると嬉しい！イケてな～いなところもあるかもしれないけどうるさいいわずに盛り上がるしてくれると嬉しい！"
+        let expected = "はむはー！このライブラリはHamuLanguageTranslatorというんだ！君の入力したテキストをハム語に変換するよ！君のワンダちゅなアプリとあわせっちしてくれるとうれぴっプル！うにに～なところもあるかもしれないけどがちゃちゃいわずにドンチャカしてくれるとうれぴっプル！"
 
 
         let result = HamuLanguageTranslator.translate(from: sources)
